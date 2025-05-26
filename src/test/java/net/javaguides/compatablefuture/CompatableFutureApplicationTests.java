b0230dbb639b4a81c77a9b0bab77e1786d6e6361
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-05-26 16:46:04 -0500

    Initall commit

--- src/test/java/net/javaguides/compatablefuture/CompatableFutureApplicationTests.java
+++ src/test/java/net/javaguides/compatablefuture/CompatableFutureApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CompatableFutureApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
