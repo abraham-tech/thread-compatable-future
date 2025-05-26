3280ed6e2f90b26fd6d608690bb99800bbca3391
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-05-26 16:45:49 -0500

    first commit

---
+++ src/main/java/net/javaguides/compatablefuture/CompatableFutureApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.compatablefuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompatableFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompatableFutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguides/compatablefuture/CompatableFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.compatablefuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompatableFutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
